ff84366641881fb419851df0d2231e6b33b95432
mladen <mladen@laptowski.localdomain>
2017-08-30 21:17:13 +0200

    fixing bugs

--- src/swagger_service/endpoint/service.clj
+++ src/swagger_service/endpoint/service.clj
@@ -30,7 +30,7 @@
                     :return       User
                     :path-params [id :- String]
                     :summary      "returns the user with a given id"
-                    (ok (db/get-users {:id id} (:db config))))
+                    (ok (first (db/get-user {:id id} (:db config)))))
 
               (POST* "/authenticate" []
                      :return      Boolean
